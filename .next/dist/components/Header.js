88a056dae75821df7197fc643db04b9ff7862733
bear2u@naver.com <kangse1004>
2018-02-26 00:37:15 +0900

    'day5'

---
+++ .next/dist/components/Header.js
@@ -0,0 +1,43 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+var _semanticUiReact = require('semantic-ui-react');
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var _jsxFileName = 'E:\\workspaces12\\kickstarter-blockchain-exam\\components\\Header.js';
+
+exports.default = function () {
+    return _react2.default.createElement(_semanticUiReact.Menu, { style: { marginTop: '10px' }, __source: {
+            fileName: _jsxFileName,
+            lineNumber: 6
+        }
+    }, _react2.default.createElement(_semanticUiReact.Menu.Item, {
+        __source: {
+            fileName: _jsxFileName,
+            lineNumber: 7
+        }
+    }, 'CrowdCoin'), _react2.default.createElement(_semanticUiReact.Menu.Menu, { position: 'right', __source: {
+            fileName: _jsxFileName,
+            lineNumber: 10
+        }
+    }, _react2.default.createElement(_semanticUiReact.Menu.Item, {
+        __source: {
+            fileName: _jsxFileName,
+            lineNumber: 11
+        }
+    }, 'Campaigns'), _react2.default.createElement(_semanticUiReact.Menu.Item, {
+        __source: {
+            fileName: _jsxFileName,
+            lineNumber: 15
+        }
+    }, '+')));
+};
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImNvbXBvbmVudHNcXEhlYWRlci5qcyJdLCJuYW1lcyI6WyJSZWFjdCIsIk1lbnUiLCJtYXJnaW5Ub3AiXSwibWFwcGluZ3MiOiI7Ozs7OztBQUFBLEFBQU87Ozs7QUFDUCxBQUFTLEFBRVQ7Ozs7OztrQkFBZSxZQUFNLEFBQ2pCOzJCQUNJLEFBQUMsdUNBQUssT0FBTyxFQUFFLFdBQWYsQUFBYSxBQUFhO3NCQUExQjt3QkFBQSxBQUNJO0FBREo7S0FBQSxrQkFDSyxjQUFELHNCQUFBLEFBQU07O3NCQUFOO3dCQUFBO0FBQUE7QUFBQSxPQURKLEFBQ0ksQUFHQSw4QkFBQyxjQUFELHNCQUFBLEFBQU0sUUFBSyxVQUFYLEFBQW9CO3NCQUFwQjt3QkFBQSxBQUNJO0FBREo7dUJBQ0ssY0FBRCxzQkFBQSxBQUFNOztzQkFBTjt3QkFBQTtBQUFBO0FBQUEsT0FESixBQUNJLEFBSUEsOEJBQUMsY0FBRCxzQkFBQSxBQUFNOztzQkFBTjt3QkFBQTtBQUFBO0FBQUEsT0FWWixBQUNJLEFBSUksQUFLSSxBQUlmO0FBZkQiLCJmaWxlIjoiSGVhZGVyLmpzIiwic291cmNlUm9vdCI6IkU6L3dvcmtzcGFjZXMxMi9raWNrc3RhcnRlci1ibG9ja2NoYWluLWV4YW0ifQ==
---
+++ .next/dist/components/Layout.js
@@ -0,0 +1,34 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+var _semanticUiReact = require('semantic-ui-react');
+
+var _Header = require('./Header');
+
+var _Header2 = _interopRequireDefault(_Header);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var _jsxFileName = 'E:\\workspaces12\\kickstarter-blockchain-exam\\components\\Layout.js';
+
+exports.default = function (props) {
+    return _react2.default.createElement(_semanticUiReact.Container, {
+        __source: {
+            fileName: _jsxFileName,
+            lineNumber: 7
+        }
+    }, _react2.default.createElement(_Header2.default, {
+        __source: {
+            fileName: _jsxFileName,
+            lineNumber: 8
+        }
+    }), props.children);
+};
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImNvbXBvbmVudHNcXExheW91dC5qcyJdLCJuYW1lcyI6WyJSZWFjdCIsIkNvbnRhaW5lciIsIkhlYWRlciIsInByb3BzIiwiY2hpbGRyZW4iXSwibWFwcGluZ3MiOiI7Ozs7OztBQUFBLEFBQU87Ozs7QUFDUCxBQUFTOztBQUNULEFBQU8sQUFBWSxBQUVuQjs7Ozs7Ozs7a0JBQWUsaUJBQVMsQUFDcEI7MkJBQ0ksQUFBQzs7c0JBQUQ7d0JBQUEsQUFDSTtBQURKO0FBQUEsS0FBQSxrQkFDSSxBQUFDOztzQkFBRDt3QkFESixBQUNJLEFBQ0M7QUFERDtBQUFBLGNBRlIsQUFDSSxBQUVXLEFBR2xCO0FBUEQiLCJmaWxlIjoiTGF5b3V0LmpzIiwic291cmNlUm9vdCI6IkU6L3dvcmtzcGFjZXMxMi9raWNrc3RhcnRlci1ibG9ja2NoYWluLWV4YW0ifQ==
